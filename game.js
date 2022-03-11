1934bd5c970bbb620d70e5f8ae40de0c2c5515ff
Ifeoluwa Adebisi <adebisiife@gmail.com>
2022-03-11 00:37:01 -0500

    Card layout function

--- game.js
+++ game.js
@@ -1,70 +1,56 @@
 // Global Variables
 
-const memeArray = [
-    // Ohhhhh
-        'https://lh3.googleusercontent.com/u5j-lWpcu37LoA5eelPayT645eW1Zue03BtH8zhX8r-DOFnCL4pJT_ODNDcdWfXcusjWm0T7y0J0UrFCe5nK3egG4a06Jy8GC-hdKXk14-m2b0aUTaA2FC8j0SVZUJxLpojgfNrQ39GSlrFR5YitanWylOlnEgibvCtbp1ELG_7iMUH0PIn-EWmjWSfhvxK2JYZdSZXCf7_RsgKB5q1HbiMfyr_AhoGXynOV26IakkSWzLt7xYlXgY1hwDXc-cy2SQuIXOQ6P3kBjjXM5yzvKDxP05sUN8mrP2z3pGdmXAUfVZmo_8xmHOzFKvGiYyt0JXynN4gb-z4aWc1f7i60kUnXnKqzim8OhK2kkvprB2xwpsCjsKYg2AJ_E3WZtLh3g7JRpKBmTFCkBS9UFEYlA4xHMd_-AdKFRZ5ERXA9Nb091RjxCwyeltVfzI-CiKuN16XX5jEOkAg1u-ykhs4s7TdgUSoTXAeKX47JJeeuvQgT1G6duDWT2cv7ZSUK_bjpy-y-657YoyoQgvzcwHcaEzE2od3M5YmA3bp3Kc_VaWrQaE6Wq2ARQ0bbRpdr3dkVcLs4piZoO_2ChSPDQyLkECNJmZtOZKHc3aWAJEZIKX9nS1pUAFmf7VEPX3f_cHFguzQzYu_d8TJfxhXWhaFiyZsr8Vnyp0iVMwujEVcc99lNYXT7ncNQGJ4ROu5A5rOYUKDQdY4-CPTSWXNvPTANUqo=w500-h376-no?authuser=0',
-    // lisa Simspson
-        'https://lh3.googleusercontent.com/IeBwCKGOLix9fuRqPmY1U001F29Ijzp9E3Rygw1niqaIrmyQGX1b5VsEQVmbpW29VVUkTvtBshlgvW4o1E9wSHoiDBH6Z6UNmxmmFeCK3sUL120gMIVxesz-sgtt5rP6L2H_86ovbMLDdRucWRxrPJYVdGb_8ZEtBuf4mfqJWEkobkRJfkrI7xYCPaAXzdwX3h0pWdqo56JSz_q9L9vlzyN8NAQbAl4Z6wQPj0rOT9H_PwdbVbS3RnnKj22g4mddjptG-JWWyg2KO2WSgkUQApNJbB2EF8s5MWhRFMxkEk6g4oNSXnxfoWCIF-l9WlgsmQOAyCnz2WPOsQz3uQ7DlX-FJACAOkX2o3YQdc7soctoSC2hx19spQXt2dm_jDa0OOFjGvCo0Kyb91u4skZa4Hw6h4wtswe4RVYeCyHbZz1UNeuVVqjPMwJ2_7SkzV16trieI3-nDf-_rI2k0EaxgZvCaulxO8mtDdjEjg7G11YJDFMW7Mh612FLQzgF2MsNXMPlglcb7n8PZof8CagQ32BwpNAIbSgDJvxj1RqaEQiynd7LueZ8MbjK0AIuTN5UH30IV_kcL_WV29xJwwPj2iLXeeAJss-WwauJRVr-ajbdvz4CLYS00F4YdOLTmceRxNPnlkTFKzXThlHqyy_FzSw0Nnsk9rkypKXUnY08V4WNks-JnbEBFoSySnlqIgrKelPDSDXBxsWRMnhU6yFX2bI=w800-h450-no?authuser=0',
-    // Prof Umar
-        'https://lh3.googleusercontent.com/QrBnKZ4JCBFh4vzjcdrqvIhTfuhJP0suT_4xKozOb7GpV6C4nUNCgMWwnD3diLEEIOmhUq2u6_kiJP5l37cIYji8TV-AfgVGPl_WBjE6sCZXr63ZQp_0UhWQPIOhIHBsiBqye9LkMwF5GOcmmHIw7ZvpGXJcgJIRsh6g7VJblJVBU-7K55pl7n-JWQogARbgC9_z6xvaJzGtjwc9eg4DgO5mI8ts8b6phnMVdIl3BYtJrwPnFAIR7R9XyAjYznVKewD0l8GmBW3PWdzUK8o1vWpi01QIEX-ula7r5xPpmKsP6zzK_L1pL7NZf10zd85rNHFfrsdPgT4C7Lt16RSCcidSq0klUCJSy_udlppJOzDhkDiyItZ_JO1wHAQu-rTIzrOerTpdEGlQDEmiSPzyd8zWBcp8vMxQoYfr_3EXT1TZ-wbLMShOdZ-lRxg5W4wLqJsqhlI9d6cN2wJEA5y7ONrUCltEQOvrxFuPJr39tww79XWN1n05eaa0JNdsO4cfIfTCx_navEhGdTWSSPLAUvIsC8ul9JD3QgwODL_sSqHQ9sLc3wKjb5kpic-NAQfAAa-U5_ktPjIEWDyRHYYU7PWJL2DTwkbsUn10zkSBS0HIfGrke-GhD62uJd-kpHxZsOIdc_dWsvVH-R5WjpF2tVXR6fSvElCFdRScfngBOYYoVGZOx5OXt3GWpXF9EhEinTJt_oFAgbkPNLylcyFS1pQ=w567-h565-no?authuser=0',
-    // tom
-        'https://lh3.googleusercontent.com/-5wnbiIhSQBpfCZVX0GftGA5aNV5VAY572ENw5SzNAVfWNnWmf40peHSlDAQwZCxESzQVZZdXOoEpgis22TaeAOxJOJXWbHMtHHLPIMHVHErUy0TUN2tp5vbNRSYD3Cxj2_mIfw8ucQhFlG1pNU13ul53u2GsLXTzeMKPJuMqo3FY6-sxJMjIGLj_t4QNi9ENpJALCTRmwkTOYGt6GWRKXrysEuB7MGMikwVvor8ncLUVUSj1N28LaGioq58CP-pgl1GKK41XtjVeIu-46Csa-HR3yP3Iw9d2NyblRYudGw2qnKGMobjqw-pJkLtk762jq546ivWYi3-1vi2sApzZsZWE_X3iuj2SgiuwnAPNczz0vRVIwz8MENRWqe7LBMzkKjEWI2iv4xzRtNbaWWcCnbO7DF3R5fYD7TJScp9elQX_Mu6NsOB8UhFH-HyA1Qg6KucjhmZAtBJUiRYJvGevIxoBmrWQf35nFxlHZoMEwa_F6SWqiH-l_G2XB1vTxaJm-6xfDbrNJUZEeTssmQ2YQb9R3doX2BXOXHSici3teYJpiKAIl3GlTIx2uU4VqwKLSDnqOO6C5mfFN-vX9-EBTenovtlZZrcNOEo8khPXixsDGpKttjPtOmRZwnMwl0nquThiyFWYi9tR9OSbOnxbmgWSw1TiHfr7duo5kH-c4yEzwxeU96lTaK2eeYqd0sg5Y5CbepPj0v2ZOTZWoDFK1Y=w567-h571-no?authuser=0',
-    // milly rock
-        'https://lh3.googleusercontent.com/Hgxh4S3EpcvmMTqmQXEudrCjmPtok3dUuYpQx4A3vVD9Hdmcr_p01bxe3wOju7nvFM-2p9Q21gao78nJTvxs3RwfsTKcoEDO0TzTI7MelUbRyBmNlxKcSNLI1eEcUSh9jYfhlK6DGfDP8jKgOA08oLdfEu0TFKfXT5j-S6fKrufk0drLBJ2lW5SnlRI8c608ZQYZ_7wcdKZcyeqcC7nSJIAu24YNmRWaaqRCINcGWmtj_Im_XGo4c_1hNbb9qrbZpsk2-KtS599xL3J8AqUkJKx45eVOyPR4BHblwXx3Q6V2nMalseYWVkCJKwiy0w7TyQndtjdLnJWijmAwGgLNUP5zAvUb0_YNA_h1yw-7PMwVK2fzYb-TLqdhdzQGIY5kY9u938bAd9_-rrPuL31ukCTs9eXob3qEcJJ-_Jp08VnI4z2NGzObK-RL0O2wf2BBmJNfTouAPeOV7zhyvfA8I5m0j55kPgpom4YjHROVoQA4XlQNiowbbKfS9Ro4zlvRR7ypho4iInF3GDZbUqkc8spDn7PnogOS7bLz5i1GruvFlvANyXK5m8kKfFXjHDD_Pt0q1_7hOpJjC73kBAzbvffkh7HIs1OJa-JYcypbpe9N6uNySs8xYiu9I_JP0uERjUi4fhq9xdsXjV5icSq8bBfpL4EUKB8awTDi6QpNBbtE92zXMRcX5oIloDTWPjLMOymzFc0C0wyUrPpsFzmEajc=w567-h433-no?authuser=0',
-    // lebron
-        'https://lh3.googleusercontent.com/hRZjI8T2AK4D32S6nDrZGEi2qnKz_XkulX8yWDOilUw7mddnfZ22MTka8yJHC5DCfznPTrH0fdYJtcLOwwJGWyiChqG2z_OubWWBc8qSvwQXfvhjN34CF7Ri8riOZVZt5j3ZEc2l5BbpeEA_VxtKdxbLUmlFPMD8NQ2j9LYliu-j0g0hBEkmugx0tIXN_U19tirZgukDrUrAF3gv-ONpZlYVHoJ8aR1OUROVODMlSR39lRlvC_GlquzHvFP2QDTztSz6hVLNrGGAeYXBh-Dkn-AMk2bzcvQAKPYoYhulkQgYLYmYUatodCvzaWC7FjuTFKg6Sfk0OTsA_G-7bjiWJrFV9uy2K6zBb-aH-H6SM9ndWaUZNtZttfq_DtZ_ufeub5ljuV3bg-r-TKJxNfLM9d8qwabNow7May-unGwGYhPJsnQvVZRGH9FFPl9e4xwLuiIhzcpX5z2E70WGMIW13-R6XIqg1aXNimgiq2CWvyQZb-HOoigRoLvpS0AnteF23ASiBWvDKijU0yfF-fXyeV3S0_c_jXBx3W0jCbpcUGkNgmktgKMlqlh8iqu2O8-Pvx_w8hGVmXY2_XH-5U_XWb9jWMTXyraHcRvVqq0K-Smx96diApIPsm2tSahEtM5mD_yn2xtTQe28Fp0e3W57TvscLJSlhg0_eMTuAItlH_9i8PL9h2sarPau_0DHP8G7EKqw4BiXvyFZdLwDqOqW3Ko=w933-h934-no?authuser=0',
-    // camera'
-        'https://lh3.googleusercontent.com/tntMruUfIDTqs8QwQ6PQ9BB91PRZvtnDBbMimY92vQHZV5ZpohvxwVYKyfBtA9Wr0zk0liuc3QKePsPPrhxAFr5TZntiPBGB4XVpPObZ2G8eC1Mo66m-0eQEaSfRECe1xpErBqshKQ-zeURfDIFFw2gD7o7qEZnnPFzmRwSUPCKEv8mm8ISL6D78LDajDea5mdAJaVjIvlaZwuqMbqDCerRn4PkdvW50xGn_LuU5JnLNCBMfCWHyALQgiSPIYMbtr1qfOo9TIIr-JjFXl9ShTg5BfipfiRLzbkiC1mSpPnn5ziS-qDqKMZzDXjNQl6LJytg1hoMfchGLjeBcK3fVQzuLWW_CgQEcHR8eiFhUpiD-huLH4lbwklpJx4nB2UlSzVw9xxkOZnDI9RfS2CNh3DT3bfvwE_ALPWR8Sc4bPG55-KtFAVVZn9hyZbi73sgQZQIlBS06-fMFMPV1SdSZ9CZQvaEFYXtqeyxzquAsS26QMDaE6_R127VWBvOqVbT3n47aWuSgMO3_JPm4OhzGtSI6b4ArT8yRQdhQz2aSBnXq88Lz4sTdTDIhyihsAG7Qx624MxTOJh0d55PqBHUgHppQdrHpt5bqqveFIzN620YGbiE8yJ-CV5R2pEky1MeS4Z33QZcMFghPXrioX4NfUzBRX_y_YUhmtmcUbUgNImbGj-XiPRwejMZvQNNxxUL6OXDkn0_eGGcwyJz_JnHEyP0=w567-h319-no?authuser=0',
-    // crying
-        'https://lh3.googleusercontent.com/AOZNWtMCa4ya5chWD4w_nCl7ny1bGmxTHSMTbYeH3EZOP0M3FLXrJG2QXz0CCb4MWXao0dTP0kTjKoqIaHY3rvDTUEiS7kLMstJGMvgvtHivIddt5Uff9ATlR0X694nRRhZH-d7Jk5IBiKLH7P4Vy2bvJd0QsTtXbClwSKs8UOEcSfIgXqeiJgkl_Q548nUjjhF0Zf9arHkhK6f6iKuMzbfgr5HY3RiON2VSewOVfHhRoknEBgkC5sjJVQYEAkqr3RDWcwOMVXuU0MXmXDVUprfZ4DaBWEhL3XrXGPVaUoXGHtZbaqcz2TsIvf5-6jyj49iGI7I6uf_5JqSP3iRwbzMpwM77BGR7ckbYSSopdR3XqU6nGoAdhmH6Q1ngERvSJMTgKNFTZYqFmTWruaDrkYl2EldXJdhDkrW9RhmhJL-KxhMxB2srrxHUiWYXP26S6w39sRxzDUV4TEakc5MsNwZ5YvtJYbwrX3O9oG4-L1UQIyhdFStnsqFX1K88PnNqCTJs4hQJE130EYo93TEzxAQHf2bDEITCb69Ne3mFV09lZnsUJb3pfL404oJKIW2Iq3q1UIcnQpD9sy_KknxzuNBWszqwDHqAlBluYkoFnmydsI5mB3KyO16fCxnX0DXsV4bNsniHHkHyvYtF3w5b2hhxtIrNy1HNfQSSxc371h6l_trhxH0VVCOiJaPinV8cnv_hI-izc1Sx4R5Tv8B7nf4=s567-no?authuser=0',
-    // Ohhhhh
-        'https://lh3.googleusercontent.com/u5j-lWpcu37LoA5eelPayT645eW1Zue03BtH8zhX8r-DOFnCL4pJT_ODNDcdWfXcusjWm0T7y0J0UrFCe5nK3egG4a06Jy8GC-hdKXk14-m2b0aUTaA2FC8j0SVZUJxLpojgfNrQ39GSlrFR5YitanWylOlnEgibvCtbp1ELG_7iMUH0PIn-EWmjWSfhvxK2JYZdSZXCf7_RsgKB5q1HbiMfyr_AhoGXynOV26IakkSWzLt7xYlXgY1hwDXc-cy2SQuIXOQ6P3kBjjXM5yzvKDxP05sUN8mrP2z3pGdmXAUfVZmo_8xmHOzFKvGiYyt0JXynN4gb-z4aWc1f7i60kUnXnKqzim8OhK2kkvprB2xwpsCjsKYg2AJ_E3WZtLh3g7JRpKBmTFCkBS9UFEYlA4xHMd_-AdKFRZ5ERXA9Nb091RjxCwyeltVfzI-CiKuN16XX5jEOkAg1u-ykhs4s7TdgUSoTXAeKX47JJeeuvQgT1G6duDWT2cv7ZSUK_bjpy-y-657YoyoQgvzcwHcaEzE2od3M5YmA3bp3Kc_VaWrQaE6Wq2ARQ0bbRpdr3dkVcLs4piZoO_2ChSPDQyLkECNJmZtOZKHc3aWAJEZIKX9nS1pUAFmf7VEPX3f_cHFguzQzYu_d8TJfxhXWhaFiyZsr8Vnyp0iVMwujEVcc99lNYXT7ncNQGJ4ROu5A5rOYUKDQdY4-CPTSWXNvPTANUqo=w500-h376-no?authuser=0',
-    // lisa Simspson
-        'https://lh3.googleusercontent.com/IeBwCKGOLix9fuRqPmY1U001F29Ijzp9E3Rygw1niqaIrmyQGX1b5VsEQVmbpW29VVUkTvtBshlgvW4o1E9wSHoiDBH6Z6UNmxmmFeCK3sUL120gMIVxesz-sgtt5rP6L2H_86ovbMLDdRucWRxrPJYVdGb_8ZEtBuf4mfqJWEkobkRJfkrI7xYCPaAXzdwX3h0pWdqo56JSz_q9L9vlzyN8NAQbAl4Z6wQPj0rOT9H_PwdbVbS3RnnKj22g4mddjptG-JWWyg2KO2WSgkUQApNJbB2EF8s5MWhRFMxkEk6g4oNSXnxfoWCIF-l9WlgsmQOAyCnz2WPOsQz3uQ7DlX-FJACAOkX2o3YQdc7soctoSC2hx19spQXt2dm_jDa0OOFjGvCo0Kyb91u4skZa4Hw6h4wtswe4RVYeCyHbZz1UNeuVVqjPMwJ2_7SkzV16trieI3-nDf-_rI2k0EaxgZvCaulxO8mtDdjEjg7G11YJDFMW7Mh612FLQzgF2MsNXMPlglcb7n8PZof8CagQ32BwpNAIbSgDJvxj1RqaEQiynd7LueZ8MbjK0AIuTN5UH30IV_kcL_WV29xJwwPj2iLXeeAJss-WwauJRVr-ajbdvz4CLYS00F4YdOLTmceRxNPnlkTFKzXThlHqyy_FzSw0Nnsk9rkypKXUnY08V4WNks-JnbEBFoSySnlqIgrKelPDSDXBxsWRMnhU6yFX2bI=w800-h450-no?authuser=0',
-    // Prof Umar
-        'https://lh3.googleusercontent.com/QrBnKZ4JCBFh4vzjcdrqvIhTfuhJP0suT_4xKozOb7GpV6C4nUNCgMWwnD3diLEEIOmhUq2u6_kiJP5l37cIYji8TV-AfgVGPl_WBjE6sCZXr63ZQp_0UhWQPIOhIHBsiBqye9LkMwF5GOcmmHIw7ZvpGXJcgJIRsh6g7VJblJVBU-7K55pl7n-JWQogARbgC9_z6xvaJzGtjwc9eg4DgO5mI8ts8b6phnMVdIl3BYtJrwPnFAIR7R9XyAjYznVKewD0l8GmBW3PWdzUK8o1vWpi01QIEX-ula7r5xPpmKsP6zzK_L1pL7NZf10zd85rNHFfrsdPgT4C7Lt16RSCcidSq0klUCJSy_udlppJOzDhkDiyItZ_JO1wHAQu-rTIzrOerTpdEGlQDEmiSPzyd8zWBcp8vMxQoYfr_3EXT1TZ-wbLMShOdZ-lRxg5W4wLqJsqhlI9d6cN2wJEA5y7ONrUCltEQOvrxFuPJr39tww79XWN1n05eaa0JNdsO4cfIfTCx_navEhGdTWSSPLAUvIsC8ul9JD3QgwODL_sSqHQ9sLc3wKjb5kpic-NAQfAAa-U5_ktPjIEWDyRHYYU7PWJL2DTwkbsUn10zkSBS0HIfGrke-GhD62uJd-kpHxZsOIdc_dWsvVH-R5WjpF2tVXR6fSvElCFdRScfngBOYYoVGZOx5OXt3GWpXF9EhEinTJt_oFAgbkPNLylcyFS1pQ=w567-h565-no?authuser=0',
-    // tom
-        'https://lh3.googleusercontent.com/-5wnbiIhSQBpfCZVX0GftGA5aNV5VAY572ENw5SzNAVfWNnWmf40peHSlDAQwZCxESzQVZZdXOoEpgis22TaeAOxJOJXWbHMtHHLPIMHVHErUy0TUN2tp5vbNRSYD3Cxj2_mIfw8ucQhFlG1pNU13ul53u2GsLXTzeMKPJuMqo3FY6-sxJMjIGLj_t4QNi9ENpJALCTRmwkTOYGt6GWRKXrysEuB7MGMikwVvor8ncLUVUSj1N28LaGioq58CP-pgl1GKK41XtjVeIu-46Csa-HR3yP3Iw9d2NyblRYudGw2qnKGMobjqw-pJkLtk762jq546ivWYi3-1vi2sApzZsZWE_X3iuj2SgiuwnAPNczz0vRVIwz8MENRWqe7LBMzkKjEWI2iv4xzRtNbaWWcCnbO7DF3R5fYD7TJScp9elQX_Mu6NsOB8UhFH-HyA1Qg6KucjhmZAtBJUiRYJvGevIxoBmrWQf35nFxlHZoMEwa_F6SWqiH-l_G2XB1vTxaJm-6xfDbrNJUZEeTssmQ2YQb9R3doX2BXOXHSici3teYJpiKAIl3GlTIx2uU4VqwKLSDnqOO6C5mfFN-vX9-EBTenovtlZZrcNOEo8khPXixsDGpKttjPtOmRZwnMwl0nquThiyFWYi9tR9OSbOnxbmgWSw1TiHfr7duo5kH-c4yEzwxeU96lTaK2eeYqd0sg5Y5CbepPj0v2ZOTZWoDFK1Y=w567-h571-no?authuser=0',
-    // milly rock
-        'https://lh3.googleusercontent.com/Hgxh4S3EpcvmMTqmQXEudrCjmPtok3dUuYpQx4A3vVD9Hdmcr_p01bxe3wOju7nvFM-2p9Q21gao78nJTvxs3RwfsTKcoEDO0TzTI7MelUbRyBmNlxKcSNLI1eEcUSh9jYfhlK6DGfDP8jKgOA08oLdfEu0TFKfXT5j-S6fKrufk0drLBJ2lW5SnlRI8c608ZQYZ_7wcdKZcyeqcC7nSJIAu24YNmRWaaqRCINcGWmtj_Im_XGo4c_1hNbb9qrbZpsk2-KtS599xL3J8AqUkJKx45eVOyPR4BHblwXx3Q6V2nMalseYWVkCJKwiy0w7TyQndtjdLnJWijmAwGgLNUP5zAvUb0_YNA_h1yw-7PMwVK2fzYb-TLqdhdzQGIY5kY9u938bAd9_-rrPuL31ukCTs9eXob3qEcJJ-_Jp08VnI4z2NGzObK-RL0O2wf2BBmJNfTouAPeOV7zhyvfA8I5m0j55kPgpom4YjHROVoQA4XlQNiowbbKfS9Ro4zlvRR7ypho4iInF3GDZbUqkc8spDn7PnogOS7bLz5i1GruvFlvANyXK5m8kKfFXjHDD_Pt0q1_7hOpJjC73kBAzbvffkh7HIs1OJa-JYcypbpe9N6uNySs8xYiu9I_JP0uERjUi4fhq9xdsXjV5icSq8bBfpL4EUKB8awTDi6QpNBbtE92zXMRcX5oIloDTWPjLMOymzFc0C0wyUrPpsFzmEajc=w567-h433-no?authuser=0',
-    // lebron
-        'https://lh3.googleusercontent.com/hRZjI8T2AK4D32S6nDrZGEi2qnKz_XkulX8yWDOilUw7mddnfZ22MTka8yJHC5DCfznPTrH0fdYJtcLOwwJGWyiChqG2z_OubWWBc8qSvwQXfvhjN34CF7Ri8riOZVZt5j3ZEc2l5BbpeEA_VxtKdxbLUmlFPMD8NQ2j9LYliu-j0g0hBEkmugx0tIXN_U19tirZgukDrUrAF3gv-ONpZlYVHoJ8aR1OUROVODMlSR39lRlvC_GlquzHvFP2QDTztSz6hVLNrGGAeYXBh-Dkn-AMk2bzcvQAKPYoYhulkQgYLYmYUatodCvzaWC7FjuTFKg6Sfk0OTsA_G-7bjiWJrFV9uy2K6zBb-aH-H6SM9ndWaUZNtZttfq_DtZ_ufeub5ljuV3bg-r-TKJxNfLM9d8qwabNow7May-unGwGYhPJsnQvVZRGH9FFPl9e4xwLuiIhzcpX5z2E70WGMIW13-R6XIqg1aXNimgiq2CWvyQZb-HOoigRoLvpS0AnteF23ASiBWvDKijU0yfF-fXyeV3S0_c_jXBx3W0jCbpcUGkNgmktgKMlqlh8iqu2O8-Pvx_w8hGVmXY2_XH-5U_XWb9jWMTXyraHcRvVqq0K-Smx96diApIPsm2tSahEtM5mD_yn2xtTQe28Fp0e3W57TvscLJSlhg0_eMTuAItlH_9i8PL9h2sarPau_0DHP8G7EKqw4BiXvyFZdLwDqOqW3Ko=w933-h934-no?authuser=0',
-    // camera'
-        'https://lh3.googleusercontent.com/tntMruUfIDTqs8QwQ6PQ9BB91PRZvtnDBbMimY92vQHZV5ZpohvxwVYKyfBtA9Wr0zk0liuc3QKePsPPrhxAFr5TZntiPBGB4XVpPObZ2G8eC1Mo66m-0eQEaSfRECe1xpErBqshKQ-zeURfDIFFw2gD7o7qEZnnPFzmRwSUPCKEv8mm8ISL6D78LDajDea5mdAJaVjIvlaZwuqMbqDCerRn4PkdvW50xGn_LuU5JnLNCBMfCWHyALQgiSPIYMbtr1qfOo9TIIr-JjFXl9ShTg5BfipfiRLzbkiC1mSpPnn5ziS-qDqKMZzDXjNQl6LJytg1hoMfchGLjeBcK3fVQzuLWW_CgQEcHR8eiFhUpiD-huLH4lbwklpJx4nB2UlSzVw9xxkOZnDI9RfS2CNh3DT3bfvwE_ALPWR8Sc4bPG55-KtFAVVZn9hyZbi73sgQZQIlBS06-fMFMPV1SdSZ9CZQvaEFYXtqeyxzquAsS26QMDaE6_R127VWBvOqVbT3n47aWuSgMO3_JPm4OhzGtSI6b4ArT8yRQdhQz2aSBnXq88Lz4sTdTDIhyihsAG7Qx624MxTOJh0d55PqBHUgHppQdrHpt5bqqveFIzN620YGbiE8yJ-CV5R2pEky1MeS4Z33QZcMFghPXrioX4NfUzBRX_y_YUhmtmcUbUgNImbGj-XiPRwejMZvQNNxxUL6OXDkn0_eGGcwyJz_JnHEyP0=w567-h319-no?authuser=0',
-    // crying
-        'https://lh3.googleusercontent.com/AOZNWtMCa4ya5chWD4w_nCl7ny1bGmxTHSMTbYeH3EZOP0M3FLXrJG2QXz0CCb4MWXao0dTP0kTjKoqIaHY3rvDTUEiS7kLMstJGMvgvtHivIddt5Uff9ATlR0X694nRRhZH-d7Jk5IBiKLH7P4Vy2bvJd0QsTtXbClwSKs8UOEcSfIgXqeiJgkl_Q548nUjjhF0Zf9arHkhK6f6iKuMzbfgr5HY3RiON2VSewOVfHhRoknEBgkC5sjJVQYEAkqr3RDWcwOMVXuU0MXmXDVUprfZ4DaBWEhL3XrXGPVaUoXGHtZbaqcz2TsIvf5-6jyj49iGI7I6uf_5JqSP3iRwbzMpwM77BGR7ckbYSSopdR3XqU6nGoAdhmH6Q1ngERvSJMTgKNFTZYqFmTWruaDrkYl2EldXJdhDkrW9RhmhJL-KxhMxB2srrxHUiWYXP26S6w39sRxzDUV4TEakc5MsNwZ5YvtJYbwrX3O9oG4-L1UQIyhdFStnsqFX1K88PnNqCTJs4hQJE130EYo93TEzxAQHf2bDEITCb69Ne3mFV09lZnsUJb3pfL404oJKIW2Iq3q1UIcnQpD9sy_KknxzuNBWszqwDHqAlBluYkoFnmydsI5mB3KyO16fCxnX0DXsV4bNsniHHkHyvYtF3w5b2hhxtIrNy1HNfQSSxc371h6l_trhxH0VVCOiJaPinV8cnv_hI-izc1Sx4R5Tv8B7nf4=s567-no?authuser=0'
-]
-
 // const boxes = document.querySelectorAll('.box');
 // const images = document.querySelectorAll('#img')
-
-const htmlMemespace = [
-image1 = document.querySelector('.image1'),
-image2 = document.querySelector('.image2'),
-image3 = document.querySelector('.image3'),
-image4 = document.querySelector('.image4'),
-image5 = document.querySelector('.image5'),
-image6 = document.querySelector('.image6'),
-image7 = document.querySelector('.image7'),
-image8 = document.querySelector('.image8'),
-image9 = document.querySelector('.image9'),
-image10 = document.querySelector('.image10'),
-image11 = document.querySelector('.image11'),
-image12 = document.querySelector('.image12'),
-image13 = document.querySelector('.image13'),
-image14 = document.querySelector('.image14'),
-image15 = document.querySelector('.image15'),
-image16 = document.querySelector('.image16')
-]
-
-
+const getData = [
+    { imgSrc: "https://s7.gifyu.com/images/camera.jpg"},
+    { imgSrc: "https://s7.gifyu.com/images/crying.jpg"},
+    { imgSrc: "https://s7.gifyu.com/images/lebron.jpg"},
+    { imgSrc: "https://s7.gifyu.com/images/Lisa-Simpson.jpg"},
+    { imgSrc: "https://s7.gifyu.com/images/milly-rock.jpg"},
+    { imgSrc: "https://s7.gifyu.com/images/Ohhhhh.jpg"},
+    { imgSrc: "https://s7.gifyu.com/images/prof-Umar.jpg"},
+    { imgSrc: "https://s7.gifyu.com/images/tom.jpg"},
+    { imgSrc: "https://s7.gifyu.com/images/camera.jpg"},
+    { imgSrc: "https://s7.gifyu.com/images/crying.jpg"},
+    { imgSrc: "https://s7.gifyu.com/images/lebron.jpg"},
+    { imgSrc: "https://s7.gifyu.com/images/Lisa-Simpson.jpg"},
+    { imgSrc: "https://s7.gifyu.com/images/milly-rock.jpg"},
+    { imgSrc: "https://s7.gifyu.com/images/Ohhhhh.jpg"},
+    { imgSrc: "https://s7.gifyu.com/images/prof-Umar.jpg"},
+    { imgSrc: "https://s7.gifyu.com/images/tom.jpg"},    
+];
 //Function for Game Logic
 
 // function to randomize the cards
-memeArray.sort(() => 0.5 - Math.random())
+const randomize = () => {
+    getData.sort(() => 0.5 - Math.random())
+    return getData
+};
 
+// Card layout
+const cardLayout = () => {
+    const cardData = randomize();
+    cardData.forEach((items) => {
+        const card = document.createElement("div");
+        const face = document.createElement("img");
+        const back = document.createElement("div");
+        card.classList = "card";
+        face.classList = "face"; 
+        back.classList = "back";
+        face.src = items.imgSrc;
+        // connect to html
+        section.appendChild(card);
+        card.appendChild(face);
+        card.appendChild(back); 
+    });
+};
+
+cardLayout();
 // Function to flip the card
+function flipCards() {
 
+}
 
 
 // images.forEach(function(box){
